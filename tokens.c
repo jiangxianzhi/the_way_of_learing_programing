abfed281618b86ec903d387c05637847520543f4
蒋先芝‘git config --global user.email 772423266@qq.comZZgit config --global user.name 蒋先芝‘ <772423266@qq.com>
2022-04-19 20:25:39 +0800

    learing way  back to c

---
+++ tokens.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+
+// c程序由各种令牌组成，tokens，可以是关键字，标识符，常量，字符串值，或者是一个符号
+
+int main(){
+    printf("hello , world!\n");
+
+}
+//单行注释
+/*多行注释*/
+/*关键字，
+auto 声明自动变量
+break 跳出当前循环
+case 开关语句分支
+char 声明字符新变量或函数返回值类型
+const 声明只读常量
+continue 结束当前循环，开始下一轮循环
+default 开关语句中的其他分支
+do 循环语句的循环体
+
+*/
